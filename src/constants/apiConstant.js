ee41e4ca39dd57b2f52ca49204869dde1b161148
ClaRa-git <rameau.claire.cr@gmail.com>
2025-03-03 13:49:48 +0100

    Ajout des constantes de routes

---
+++ src/constants/apiConstant.js
@@ -0,0 +1,22 @@
+// Recencement de toutes les routes de l'API
+
+// Déclaration de l'URL de base de l'API
+export const API_ROOT = 'http://localhost:8083';
+
+// Déclaration de l'URL de l'API
+export const API_URL = `${API_ROOT}/api`;
+
+// Déclaration de l'URL pour les images
+export const IMAGE_URL = `${API_ROOT}/images`;
+
+// Déclaration de l'URL pour les avatars
+export const AVATAR_URL = `${IMAGE_URL}/avatars`;
+
+// Déclaration de l'URL pour les images d'albums
+export const ALBUM_URL = `${API_ROOT}/upload/images/albums`;
+
+// Déclaration de l'URL pour les images d'artistes
+export const ARTIST_URL = `${API_ROOT}/upload/images/artists`;
+
+// Déclaration de l'URL pour les mp3
+export const MUSIC_URL = `${API_ROOT}/upload/files/music`;
