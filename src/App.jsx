d4dc76b06cf4313dce3b6b104c3c57f4273f95df
ClaRa-git <rameau.claire.cr@gmail.com>
2025-03-07 11:15:58 +0100

    Ajout de la topbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import { USER_INFOS } from './constants/appConstant'
 import useAuhtCheck from './hooks/useAuthCheck';
 import Sidebar from './components/Ui/Sidebar';
+import Topbar from './components/Ui/Topbar';
 
 const App = () => {
   // on récupère les infos de l'user dans le local storage
@@ -13,7 +14,7 @@ const App = () => {
     <div className='relative flex'>
       <Sidebar />
       <div className="flex-1 flex flex-col bg-gradient-to-b from-black to-[rgb(18,18,18)]">
-        {/* TODO: appeler la topbar */}
+        <Topbar />
         <div className='h-[calc(100vh-64px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse'>
           <div className='flex-1 h-fit pb-40 text-white'>
             <Outlet />
---
+++ src/components/Ui/Topbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Topbar = () => {
+  return (
+    <div className='h-16 flex items-center bg-green_top shadow-md'>
+        <div className="flex-1 text-white text-lg font-semibold px-4">
+            TopBar
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
